95cffef874cecbc7601b557d8d748b8198f1158e
TaeWoo <43276268+ing9990@users.noreply.github.com>
2022-02-28 08:53:04 +0900

    entityManager를 wrapping한 클래스인 JpaRepository 등등이 있지만, 성능이슈나 지원하지 않는 기능은 직접 em으로 구현할 수 있다. 결과는 3개가 output되지만 select 쿼리는 한번만 날린다. 같은 트랜잭션 내에서 한번 select가 되면 영속성 컨텍스트에 등록이 된다 다시 같은 쿼리가 날라오면 db에 쿼리를 조회하지 않고 select없이 처리할 수 있다.
    1차 캐시는 Map<id,Entity> 방식으로 저장된다.

--- src/test/java/com/fastcampus/jpa/bookmanager/service/EntityManagerTest.java
+++ src/test/java/com/fastcampus/jpa/bookmanager/service/EntityManagerTest.java
@@ -13,41 +13,38 @@ import org.springframework.transaction.annotation.Transactional;
  * @since 2021/05/13
  */
 @SpringBootTest
-@Transactional
 public class EntityManagerTest {
+
     @Autowired
-    private EntityManager entityManager;
+    private EntityManager em;
+
+    // entityManager를 wrapping한 클래스인 JpaRepository 등등이 있지만,
+    // 성능이슈나 지원하지 않는 기능은 직접 em으로 구현할 수 있다.
+
     @Autowired
     private UserRepository userRepository;
-
     @Test
-    void entityManagerTest() {
-        System.out.println(entityManager.createQuery("select u from User u").getResultList());
+    void entityManager(){
+        System.out.println(em.createQuery("select u from User u")
+                .getResultList());
     }
 
     @Test
-    void cacheFindTest() {
-//        System.out.println(userRepository.findByEmail("martin@fastcampus.com"));
-//        System.out.println(userRepository.findByEmail("martin@fastcampus.com"));
-//        System.out.println(userRepository.findByEmail("martin@fastcampus.com"));
-//        System.out.println(userRepository.findById(2L).get());
-//        System.out.println(userRepository.findById(2L).get());
-//        System.out.println(userRepository.findById(2L).get());
-//
-        userRepository.deleteById(1L);
+    void cacheFindTest(){
+        System.out.println(userRepository.findById(1L).get());
+        System.out.println(userRepository.findById(1L).get());
+        System.out.println(userRepository.findById(1L).get());
+
+        // 결과는 3개가 output되지만 select 쿼리는 한번만 날린다.
+        // 같은 트랜잭션 내에서 한번 select가 되면 영속성 컨텍스트에 등록이 된다
+        // 다시 같은 쿼리가 날라오면 db에 쿼리를 조회하지 않고 select없이 처리할 수 있다.
+
+        // 1차 캐시는 Map<id,Entity> 방식으로 저장된다.
+
+
+       /* System.out.println(userRepository.findByEmail("martin@fastcampus.com"));
+        System.out.println(userRepository.findByEmail("martin@fastcampus.com"));
+        System.out.println(userRepository.findByEmail("martin@fastcampus.com"));*/
     }
 
-    @Test
-    void cacheFindTest2() {
-        User user = userRepository.findById(1L).get();
-        user.setName("marrrrrrrtin");
-        userRepository.save(user);
-
-        System.out.println("---------------------");
-
-        user.setEmail("marrrrrrtin@fastcampus.com");
-        userRepository.save(user);
-
-        System.out.println(userRepository.findAll());       // select * from user
-    }
 }
